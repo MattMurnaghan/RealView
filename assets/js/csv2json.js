41cb51bd6945ea40a153e33173ab16eb6e572962
Matthew Murnaghan <matt.murnaghan@outlook.ie>
2022-08-10 02:43:56 +0000

    refactor csv2json to write json files

--- assets/js/csv2json.js
+++ assets/js/csv2json.js
@@ -4,27 +4,30 @@
 // The data will be converted into an array of json objects to create candlestick charts using the apexcharts node module
 
 
-import {readFileSync, promises as fsPromises, readdir} from 'fs';
+import {
+  writeFile,
+  readFileSync,
+  promises as fsPromises,
+  readdir,
+  appendFile
+} from 'fs';
 
 // Reads the name of a file and returns an array split by new line characters of the csv file data
 const syncReadFile = (filename) => {
   const contents = readFileSync(filename, 'utf-8');
   const arr = contents.split(/\r?\n/);
-  // console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
-  // console.log('length = ', arr.length)
-  // console.log('arr is ' , arr)
   return arr;
 }
 
 // Converts a unixtimestamp to a date object,  can also be used to output a formatted string for just the time without the date
 export const unixTimeStampToDateTime = (stamp) => {
-  let date = new Date(stamp); 
-  // let hours = date.getHours();      //  Hours part from the timestamp
-  // let minutes = date.getMinutes();  //  minutes part from the timestamp
-  // let seconds = date.getSeconds();  //  seconds part from the timestamp
+  let date = new Date(stamp);
+  let year = date.getFullYear();  //  seconds part from the timestamp
+  let month = date.getMonth() + 1;      //  Hours part from the timestamp
+  let day = date.getDate();  //  minutes part from the timestamp
   // // Will display time in 10:30:23 format
-  // let formattedTime = hours + ':' + minutes + ':' + seconds;
-  return date
+  let formattedTime = year + '-' + month + '-' + day;
+  return formattedTime
 }
 
 // converts a string of csv data to a kline object and returns the object as an argument
@@ -32,103 +35,70 @@ export const unixTimeStampToDateTime = (stamp) => {
 const stringToKlineObj = (arr) => {
   // Declare a kline object to be returned with populated data
   let kline = {
-      openTime: ' ',
-      open: ' ',
-      high: ' ',
-      low: ' ',
-      close: ' ',
-      volume: ' ',
-      closeTime: ' ',
-      quoteAssetVolume: ' ',
-      numberOfTrades: ' ',
-      takerBuyAssetVolume: ' ',
-      takerBuyQuoteAssetVolume: ' '
+    openTime: ' ',
+    open: ' ',
+    high: ' ',
+    low: ' ',
+    close: ' ',
+    volume: ' ',
+    closeTime: ' ',
+    quoteAssetVolume: ' ',
+    numberOfTrades: ' ',
+    takerBuyAssetVolume: ' ',
+    takerBuyQuoteAssetVolume: ' '
   };
   // loops through the passed array argument to create a kline object 
   let i = 0;
-
-  // console.log('arr')
-  // console.log(arr)
   for (const key in kline) {
-      // if (kline.hasOwnProperty(key)) {
-      //   console.log(key)
-      //   kline[key] = parseFloat(arr[i])
-      // };
-      // console.log(parseFloat(arr[i]));
-      kline[key] = parseFloat(arr[i]);
-  i++;
+    kline[key] = parseFloat(arr[i]);
+    i++;
   };
-  // console.log(kline)
 
   kline.openTime = unixTimeStampToDateTime(kline.openTime);
   kline.closeTime = unixTimeStampToDateTime(kline.closeTime);
-  return kline;
+
+  let formatted_kline = {
+    time: kline.openTime, 
+    open: kline.open,
+    high: kline.high, 
+    low: kline.low, 
+    close: kline.close 
+  }
+  return formatted_kline;
 }
 
-//TODO I think that returning an array of kline objects is the right call here, check this with guido
 // Reads a CSV file from https://data.binance.vision/?prefix=data/spot/ and returns an array of kline csv strings
-async function readKlineCsv (file) {
+async function readKlineCsv(file) {
   let arr = syncReadFile(file);
-
   let kline_string_array = Array.apply(arr.length);
-  
   let i = 0;
-  for(i=0; i < arr.length; i++){
-    kline_string_array[i]= arr[i].split(',');
+  for (i = 0; i < arr.length; i++) {
+    kline_string_array[i] = arr[i].split(',');
   }
-
-  // console.log(kline_string_array[1]);
   return kline_string_array;
 }
 
 // reads a file path to a csv document as the argument and converts the file first to an array of csv strings
 // it then converts the string array to an array of objects each holding a kline object to be read by the apex charts node module 
-async function klineObjFromCsv (file) {
+async function klineObjFromCsv(file) {
   let kline_string_array = await readKlineCsv(file);
   let kline_obj_array = Array(kline_string_array.length);
   let i = 0;
-  // console.log('first entry')
-  // console.log(kline_string_array[0])
+  let data;
 
-  for(i=0; i < kline_string_array.length; i++){
-    // console.log(i);
-    // console.log(stringToKlineObj(kline_string_array[i]));
+  for (i = 0; i < kline_string_array.length; i++) {
     kline_obj_array[i] = stringToKlineObj(kline_string_array[i]);
   }
 
-  // for(const kline_string in kline_string_array){
-  //   console.log('kline string')
-  //   console.log(kline_string[i]);
-  //   console.log('passing to stringToKlineObj')
-  //   kline_obj_array[i] = stringToKlineObj(kline_string[i]);
-  //   i++;
-  // }
-  console.log(kline_obj_array)
-  // let klineObj = stringToKlineObj(klineStringArray);
+  file = file.substring(0, file.indexOf('.')) + '.json';
+  data = JSON.stringify(kline_obj_array);
+  writeFile(file, data, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
   return kline_obj_array;
 }
-
-let file = 'assets/csv/binance_data/1h/BTCUSDT-1h-2021-01.csv';
-
-readKlineCsv(file);
-klineObjFromCsv(file);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let klineObj = klineObjFromCsv(file);
-// console.log('kline obj = ', klineObj)
 
 // asynchronous function that reads through the files in a directory and returns an array of the file names
 async function readFilesInDirectory(rootPath) {
@@ -139,28 +109,16 @@ async function readFilesInDirectory(rootPath) {
   }
 }
 
-let rootPath = 'assets/csv/binance_data/1h';
-
-// readFilesInDirectory(rootPath).then((result) => {
-//   console.log(result)
-// });
-
-// This function takes a directory as the argument and loops through the directory, reading each csv file and returning an 
-// array of kline objects for each file
-const getCandleStickData = (path_to_csv_files) => {
-  let klines = [];
+// This function takes a directory as a parameter and loops through the directory, reading each csv file and 
+// writing a json file from the historical binance csv candlestick data 
+async function getCandleStickData (path_to_csv_files) {
   let files;
   //get file names
-  readFilesInDirectory(path_to_csv_files).then((result) => {
-    // console.log(result);
-    files = result;
-
-  }).then(() =>{
-    files.forEach(element => {
-      console.log(klineObjFromCsv(`${path_to_csv_files}/${element}`));
-    });
-  })
-
+  files = await readFilesInDirectory(path_to_csv_files)
+  files.forEach(element => {
+        klineObjFromCsv(`${path_to_csv_files}/${element}`);
+      });
 }
 
-// getCandleStickData(rootPath);
+let root_path = 'assets/csv/kline_data/btcusdt/1d';
+getCandleStickData(root_path);
